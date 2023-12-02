ca19fd0e028e6b92bbfa8fca9f6ede9291a6b657
Tamim Iqbal Maruf <marufmd775@gmail.com>
2023-12-02 20:40:45 +0600

    module 11 done

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 const app: Application = express();
 
 //parsers
@@ -11,13 +11,13 @@ app.use(express.json());
 app.use(cors());
 
 // application route
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
 app.use(globalErrorHandler)
+app.use(notFound)
 
 export default app;
