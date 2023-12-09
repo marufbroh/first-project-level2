2d50edf6746440112dee31a76341f3d5aa3a8406
Tamim Iqbal Maruf <marufmd775@gmail.com>
2023-12-09 23:48:00 +0600

    error handling

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from "express";
 
-const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
     const statusCode = error.statusCode || 500;
     const message = error.message || "Something went wrong!";
+
+
+
     return res.status(statusCode).json({
         success: false,
         message,
