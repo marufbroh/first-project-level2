2b1986fcea71800fc47e74a8eae765fc3f167390
Tamim Iqbal Maruf <marufmd775@gmail.com>
2023-12-10 16:39:15 +0600

    UnhandledRejection _ UncaughtException

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
+import { Server } from 'http';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`app listening on port ${config.port}`);
     });
   } catch (error) {
@@ -16,3 +19,16 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1)
+    })
+  }
+  process.exit(1);
+})
+
+process.on('uncaughtException', () => {
+  process.exit(1)
+})
