5cda2072d9645424d65e30a614d9be5d2de7a635
Tamim Iqbal Maruf <marufmd775@gmail.com>
2023-12-19 12:34:51 +0600

    createSemesterRegistration working

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,6 +1,28 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
+import { AcademicSemester } from "../academicSemester/academicSemester.model";
 import { TSemesterRegistration } from "./semesterRegistration.interface";
+import { SemesterRegistration } from "./semesterRegistration.model";
 
 const createSemesterRegistrationIntoDB = async (payload: TSemesterRegistration) => {
+    const academicSemester = payload.academicSemester;
+
+    const isAcademicSemesterExists = await AcademicSemester.findById(academicSemester);
+    if (!isAcademicSemesterExists) {
+        throw new AppError(httpStatus.NOT_FOUND, "This academic semester not found");
+    }
+
+    const isSemesterRegistrationExists = await SemesterRegistration.findOne({
+        academicSemester
+    })
+
+    if (isSemesterRegistrationExists) {
+        throw new AppError(httpStatus.CONFLICT, "This semester is already registered!");
+    }
+
+
+    const result = await SemesterRegistration.create(payload);
+    return result;
 
 }
 
@@ -11,4 +33,4 @@ export const SemesterRegistrationService = {
     // getSingleSemesterRegistrationsFromDB,
     // updateSemesterRegistrationIntoDB,
     // deleteSemesterRegistrationFromDB,
-  };
+};
