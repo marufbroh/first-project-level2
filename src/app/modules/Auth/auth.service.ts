bacebb58454c954e23ffd2ab55e96f3b446eecdd
Tamim Iqbal Maruf <marufmd775@gmail.com>
2023-12-22 19:01:20 +0600

    needsPasswordChange operation

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,8 +2,9 @@ import httpStatus from "http-status";
 import AppError from "../../errors/AppError";
 import { User } from "../user/user.model";
 import { TLoginUser } from "./auth.interface";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../../config";
+import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: TLoginUser) => {
 
@@ -56,6 +57,55 @@ const loginUser = async (payload: TLoginUser) => {
 };
 
 
+const changePassword = async (
+    userData: JwtPayload,
+    payload: { oldPassword: string, newPassword: string }) => {
+
+    const user = await User.isUserExistsByCustomId(userData.userId);
+
+    if (!user) {
+        throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
+    }
+
+    const isDeleted = user?.isDeleted;
+
+    if (isDeleted) {
+        throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !');
+    }
+
+    const userStatus = user?.status;
+
+    if (userStatus === 'blocked') {
+        throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked ! !');
+    }
+
+    if (!(await User.isPasswordMatched(payload?.oldPassword, user?.password))) {
+        throw new AppError(httpStatus.FORBIDDEN, 'Old password do not matched');
+    }
+
+    const newHashedPassword = await bcrypt.hash(
+        payload.newPassword,
+        Number(config.bcrypt_salt_rounds)
+    )
+
+    await User.findOneAndUpdate(
+        {
+            id: userData.userId,
+            role: userData.role,
+        },
+        {
+            password: newHashedPassword,
+            needsPasswordChange: false,
+            passwordChangedAt: new Date(),
+        }
+    )
+
+    return null;
+
+};
+
+
 export const AuthServices = {
     loginUser,
+    changePassword,
 };
