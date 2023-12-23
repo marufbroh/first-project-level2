6777afcd4b22e8e339bb9b6d1fba1b8fe4651619
Tamim Iqbal Maruf <marufmd775@gmail.com>
2023-12-23 10:54:02 +0600

    mod 19 complete

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -2,24 +2,28 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { CourseControllers } from './course.controller';
 import { CourseValidations } from './course.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/create-course',
+  auth(USER_ROLE.admin),
   validateRequest(CourseValidations.createCourseValidationSchema),
   CourseControllers.createCourse,
 );
 
-router.get('/:id', CourseControllers.getSingleCourse);
+router.get('/:id', auth('student', 'faculty', 'admin'), CourseControllers.getSingleCourse);
 
 router.patch(
   '/:id',
+  auth('admin'),
   validateRequest(CourseValidations.updateCourseValidationSchema),
   CourseControllers.updateCourse,
 );
 
-router.delete('/:id', CourseControllers.deleteCourse);
+router.delete('/:id', auth('admin'), CourseControllers.deleteCourse);
 
 router.put('/:courseId/assign-faculties',
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
