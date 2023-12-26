f9f1cce1fdb66dde8dad69a4618afde69a963512
Tamim Iqbal Maruf <marufmd775@gmail.com>
2023-12-26 15:48:21 +0600

    update controller route

--- src/app/modules/EnrolledCourse/enrolledCourse.controller.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.controller.ts
@@ -6,20 +6,34 @@ import { EnrolledCourseServices } from "./enrolledCourse.service";
 const createEnrolledCourse = catchAsync(async (req, res) => {
     const userId = req.user.userId;
     const result = await EnrolledCourseServices.createEnrolledCourseIntoDB(
-      userId,
-      req.body,
+        userId,
+        req.body,
     );
-  
+
     sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is enrolled succesfully',
-      data: result,
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Student is enrolled succesfully',
+        data: result,
     });
-  });
- 
-  
-  export const EnrolledCourseControllers = {
+});
+
+const updateEnrolledCourseMarks = catchAsync(async (req, res) => {
+    const facultyId = req.user.userId;
+    const result = await EnrolledCourseServices.updateEnrolledCourseMarksIntoDB(
+        facultyId,
+        req.body,
+    );
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Marks is updated succesfully',
+        data: result,
+    });
+});
+
+export const EnrolledCourseControllers = {
     createEnrolledCourse,
-    // updateEnrolledCourseMarks,
-  };
+    updateEnrolledCourseMarks,
+};
